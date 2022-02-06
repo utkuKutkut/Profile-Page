b29127a8809a9ab99fb241e0055659e04407134e
utkuKutkut <uckutkut@dons.usfca.edu>
2022-02-06 13:46:25 -0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,14 @@ import './App.css';
 function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
-      <div className='body' >
+      <div className='body'>
         <a  href={"https://github.com/utkuKutkut/profile-page"}> Home </a>
         <h1>Profile Page </h1>
-        </div>
-        <div className='App' >
+      </div>
+      <div className='App'>
         <h1>Utku Can Kutkut </h1>
         <h4>github/utkuKutkut </h4>
-        </div>
-    
+      </div>
     </Router>
   );
 }
